7cb716f8a7805aeac497474e5c4268657fc86ec6
James Harton <james@automat.nz>
2019-12-19 13:07:56 +1300

    Bump to v0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule INA219.MixProject do
   def project do
     [
       app: :ina219,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       description:
